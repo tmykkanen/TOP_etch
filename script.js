559a4ffe2bdd7315b523600f59aa974bed361b34
tmykkanen <1270127+tmykkanen@users.noreply.github.com>
2024-04-10 09:28:50 -0500

    finished initial etch-a-sketch project

--- script.js
+++ script.js
@@ -2,23 +2,43 @@
 let flexbox;
 let i;
 const container = document.getElementById('container');
+const gridSizeBtn = document.getElementById('gridSize');
 
-for (flexbox = 1; flexbox < 17; flexbox += 1) {
-  const box = document.createElement('div');
-  box.classList.add('outer-box');
-  for (i = 1; i < 17; i += 1) {
-    const div = document.createElement('div');
-    div.classList.add('inner-box');
-    div.textContent = `${flexbox}.${i}`;
-    box.appendChild(div);
+const drawGrid = () => {
+  let size;
+
+  while (true) {
+    size = +prompt('What size grid?');
+    if (size <= 100) {
+      break;
+    } else {
+      alert('Please enter a value 100 or lower.');
+    }
   }
-  container.appendChild(box);
-}
+
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+
+  for (flexbox = 1; flexbox < (size + 1); flexbox += 1) {
+    const box = document.createElement('div');
+    box.classList.add('outer-box');
+    for (i = 1; i < (size + 1); i += 1) {
+      const div = document.createElement('div');
+      div.classList.add('inner-box');
+      box.appendChild(div);
+    }
+    container.appendChild(box);
+  }
+};
 
 // hover change
 container.addEventListener('mouseover', (e) => {
-  const target = e.target;
-  console.log(target);
-  target.classList.toggle('hover')
-
+  const { target } = e;
+  target.classList.add('draw');
 });
+
+// grid size
+gridSizeBtn.addEventListener('click', drawGrid);
+
+window.addEventListener('load', drawGrid);
